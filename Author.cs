86c91a1374067d6a5e72b9de1199e02c3eaef79c
Érica Barbosa <82848078+CB3012701@users.noreply.github.com>
2025-09-09 19:15:13 -0300

    Add files via upload

---
+++ Author.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Érica Barbosa Pereira Lobo CB3012701
+
+namespace TP01.Models
+{
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public char Gender { get; set; }
+
+        public Author(string name, string email, char gender)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"Author[name={this.Name}, email={this.Email}, gender={this.Gender}]";
+        }
+    }
+}
+
