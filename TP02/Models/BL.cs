cf567736ffa340985f9dbf34c874f30894dc1ff2
Érica Barbosa <82848078+CB3012701@users.noreply.github.com>
2025-09-24 15:21:45 -0300

    Create BL.cs

---
+++ TP02/Models/BL.cs
@@ -0,0 +1,23 @@
+//Érica Barbosa Pereira Lobo CB3012701
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CBTSWE2_TP02.Models
+{
+    public class BL
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Numero { get; set; } = string.Empty;
+
+        [Required]
+        public string Consignee { get; set; } = string.Empty;
+
+        [Required]
+        public string Navio { get; set; } = string.Empty;
+
+        public ICollection<Container> Containers { get; set; } = new List<Container>();
+    }
+}
