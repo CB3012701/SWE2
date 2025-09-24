ea9875fccdc26f77fb06f5fb4e6baf68e89fa9cd
Érica Barbosa <82848078+CB3012701@users.noreply.github.com>
2025-09-24 15:23:05 -0300

    Create Container.cs

---
+++ TP02/Models/Container.cs
@@ -0,0 +1,28 @@
+//Érica Barbosa Pereira Lobo CB3012701
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CBTSWE2_TP02.Models
+{
+    public class Container
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O número do container deve ter 11 caracteres.")]
+        public string Numero { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression("^(Dry|Reefer)$", ErrorMessage = "O tipo deve ser 'Dry' ou 'Reefer'.")]
+        public string Tipo { get; set; } = string.Empty;
+
+        [Required]
+        [Range(20, 40, ErrorMessage = "O tamanho deve ser 20 ou 40.")]
+        public int Tamanho { get; set; }
+
+        [Required]
+        public int BLId { get; set; }
+        public BL BL { get; set; } = null!;
+    }
+}
+    
