a1f921e8c1f9f09cc338f549ec2bd5857e5faafb
Érica Barbosa <82848078+CB3012701@users.noreply.github.com>
2025-12-03 18:46:53 -0300

    Create ITarefaRepository.cs

---
+++ TP04/WEB_API/Repositories /ITarefaRepository.cs
@@ -0,0 +1,17 @@
+using Models;
+
+namespace WEB_API.Repositories
+{
+    public interface ITarefaRepository
+    {
+        Task<IEnumerable<Tarefa>> GetTarefas();
+        Task<IEnumerable<Tarefa>> GetTarefasConcluidas();
+        Task<IEnumerable<Tarefa>> GetTarefasPendentes();
+        Task<Tarefa> GetTarefaById(int id);
+        Task<IEnumerable<Tarefa>> GetTarefaByTitulo(string titulo); 
+        Task AddTarefa(Tarefa tarefa);
+        Task UpdateTarefa(Tarefa tarefa);
+        Task UpdateStatusTarefa(int id);
+        Task DeleteTarefa(int id);
+    }
+}
